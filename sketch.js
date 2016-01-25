914e896cb71250eb1308959ca21490f7a85a7518
jillbook <jillbook@Jills-MacBook-Pro.local>
2016-01-24 21:33:47 -0500

    grid
    made a grid

--- sketch.js
+++ sketch.js
@@ -1,24 +1,15 @@
-/*
-  // A loop is structued like this:
-  keyword (condition) {
-    stuff that happens
-  }
-
-  // while loop
-  // (condition)
-  while (keep going as long as this evaluates to true) {
-    // stuff happens
-  }
-
-  // for loop:
-  // (variable assignment; condition to run while true; change to iterator)
-  for(var i = 0; i < 20; i = i+ 1) {
-    // stuff happens
-  }
-*/
+var x = 0;
+var y = 0;
 
 function setup() {
+createCanvas(500,500);
 
+for (var x=0; x<=500; x=x+50) {
+  ellipse(x,y,20,20);
+  for (var y=0; y<=500; y=y+50) {
+    ellipse(x,y,20,20);
+  }
+}
 }
 
 function draw() {
